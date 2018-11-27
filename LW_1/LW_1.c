73b48b1a85cd452d7f5ee3c8f63d17c42abeaef1
nikitasa1997 <nikitasa1997@gmail.com>
2018-11-27 13:22:36 +0300

    Add files via upload

--- LW_1/LW_1.c
+++ LW_1/LW_1.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <float.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -14,18 +15,22 @@
 #define DEL_X (0.5)
 #define DEL_Y (0.25)
 
-int start(int, int, size_t, dbl, size_t, dbl, int, const char * restrict);
-static int init(int, int, size_t, const size_t m_y,
-    dbl (* restrict * restrict)[m_y],
-    dbl (* restrict * restrict)[m_y],
-    dbl (* restrict * restrict)[m_y]);
-static int send_recv(int, int, size_t, size_t my, dbl (* restrict)[m_y]);
-static int send(int, int, size_t, size_t my, dbl (* restrict)[m_y]);
-static int recv(int, int, size_t, size_t my, dbl (* restrict)[m_y]);
-static int file_write(int, int, size_t, size_t my, dbl (* restrict)[m_y],
+void at_exit(void);
+void start(int, int, size_t, dbl, size_t, dbl, int, const char * restrict);
+
+static void init(int, int, size_t, size_t,
+    dbl (* restrict * restrict)[],
+    dbl (* restrict * restrict)[],
+    dbl (* restrict * restrict)[]);
+static void finalize(size_t, dbl (* restrict)[], dbl (* restrict)[],
+    dbl (* restrict)[]);
+static void send_recv(int, int, size_t, size_t, dbl (* restrict)[]);
+static void send(int, int, size_t, size_t, dbl (* restrict)[]);
+static void recv(int, int, size_t, size_t, dbl (* restrict)[]);
+static void file_write(int, int, size_t, size_t, dbl (* restrict)[],
     const char * restrict);
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
@@ -43,35 +48,30 @@ int main(int argc, const char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int int_return = start(size, rank, m_x, DEL_X, m_y, DEL_Y, N, argv[3]);
-    exit_if(int_return, err_0);
+    start(size, rank, m_x, DEL_X, m_y, DEL_Y, N, argv[3]);
 
     MPI_Finalize();
 
     return 0;
 }
 
-int start(const int size, const int rank,
+void start(const int size, const int rank,
     const size_t m_x, const dbl del_x, const size_t m_y, const dbl del_y,
     const int n, const char * const restrict filename)
 {
+    exit_if(m_x < 3U || del_x < DBL_EPSILON || isinf(del_x) || isnan(del_x)
+        || m_y < 3U || del_y < DBL_EPSILON || isinf(del_y) || isnan(del_y)
+        || size < 1 || rank < 0 || size <= rank || m_x - 2U < (size_t) size,
+        "m_x, del_x, m_y, del_y, size, rank");
+
     dbl (* restrict u_k)[m_y],
         (* restrict u_k_plus_1)[m_y],
         (* restrict r)[m_y];
-
-    int return_value = MPI_ERR_LASTCODE;
-    err_if(m_x < 3U || del_x < DBL_EPSILON || isinf(del_x) || isnan(del_x)
-        || m_y < 3U || del_y < DBL_EPSILON || isinf(del_y) || isnan(del_y)
-        || size < 1 || rank < 0 || size <= rank || m_x - 2U < (size_t) size, err_0);
-
     const size_t rows = (m_x - 2U) / size + (rank < (m_x - 2U) % size) + 2U;
+    init(size, rank, rows, m_y, &u_k, &u_k_plus_1, &r);
     const dbl rdx2 = 1.0 / del_x / del_x,
               rdy2 = 1.0 / del_y / del_y,
               beta = 1.0 / (2.0 * (rdx2 + rdy2));
-
-    return_value = init(size, rank, rows, m_y, &u_k, &u_k_plus_1, &r);
-    err_if(return_value != MPI_SUCCESS, err_0);
-
     const size_t rows_minus_1 = rows - 1,
                  m_y_minus_1 = m_y - 1;
     for (int k = 0; k < n; ++k)
@@ -90,16 +90,12 @@ int start(const int size, const int rank,
         u_k_plus_1 = (dbl (*)[m_y]) tmp_ptr;
     }
 
-    file_write();
-    return_value = 0;
-
-err_1:
-    finalize();
-err_0:
-    return return_value;
+    file_write(size, rank, rows, m_y, u_k, filename);
+    finalize(m_y, u_k, u_k_plus_1, r);
 }
 
-static int init(const int size, const int rank, const size_t rows, const size_t m_y,
+static void init(const int size, const int rank,
+    const size_t rows, const size_t m_y,
     dbl (* restrict * const restrict u_k)[m_y],
     dbl (* restrict * const restrict u_k_plus_1)[m_y],
     dbl (* restrict * const restrict r)[m_y])
@@ -107,11 +103,11 @@ static int init(const int size, const int rank, const size_t rows, const size_t 
     *u_k        = (dbl (*)[m_y]) malloc(rows * m_y * sizeof(dbl));
     *u_k_plus_1 = (dbl (*)[m_y]) malloc(rows * m_y * sizeof(dbl));
     *r          = (dbl (*)[m_y]) malloc(rows * m_y * sizeof(dbl));
-    err_if(!*u_k || !*u_k_plus_1 || !*r, err_0);
+    exit_if(!*u_k || !*u_k_plus_1 || !*r, "malloc()");
 
-    for (ptrdiff_t i = 0; i < rows; ++i)
+    for (size_t i = 0; i < rows; ++i)
     {
-        for (ptrdiff_t j = 0; j < m_y; ++j)
+        for (size_t j = 0; j < m_y; ++j)
         {
             if ((!rank && !i) || (rank + 1 == size && i + 1 == rows)
                 || !j || j + 1 == m_y)
@@ -125,121 +121,101 @@ static int init(const int size, const int rank, const size_t rows, const size_t 
             (*r)[i][j] = 0.0;
         }
     }
-
-    return MPI_SUCCESS;
-
-err_0:
-    free(*r);
-    free(*u_k_plus_1);
-    free(*u_k);
-
-    return MPI_ERR_NO_MEM;
 }
 
-static int send_recv(const int size, const int rank,
+static void finalize(const size_t m_y, dbl (* const restrict u_k)[m_y],
+    dbl (* const restrict u_k_plus_1)[m_y],
+    dbl (* const restrict r)[m_y])
+{
+    free(u_k);
+    free(u_k_plus_1);
+    free(r);
+}
+
+static void send_recv(const int size, const int rank,
     const size_t rows, const size_t m_y, dbl (* const restrict u_k)[m_y])
 {
-    int int_return = MPI_SUCCESS;
     if (rank % 2)
     {
-        int_return = send(size, rank, rows, m_y, u_k);
-        err_if(int_return != MPI_SUCCESS, err_0);
-        int_return = recv(size, rank, rows, m_y, u_k);
-        err_if(int_return != MPI_SUCCESS, err_0);
+        send(size, rank, rows, m_y, u_k);
+        recv(size, rank, rows, m_y, u_k);
     }
     else
     {
-        int_return = recv(size, rank, rows, m_y, u_k);
-        err_if(int_return != MPI_SUCCESS, err_0);
-        int_return = send(size, rank, rows, m_y, u_k);
-        err_if(int_return != MPI_SUCCESS, err_0);
+        recv(size, rank, rows, m_y, u_k);
+        send(size, rank, rows, m_y, u_k);
     }
-
-err_0:
-    return int_return;
 }
 
-static int send(const int size, const int rank,
+static void send(const int size, const int rank,
     const size_t rows, const size_t m_y, dbl (* const restrict u_k)[m_y])
 {
-    int int_return = MPI_SUCCESS;
+    int int_return;
     if (rank)
     {
         int_return = MPI_Send(u_k[1] + 1, (int) m_y - 2, MPI_DOUBLE,
             rank - 1, 0, MPI_COMM_WORLD);
-        err_if(int_return != MPI_SUCCESS, err_0);
+        exit_if(int_return != MPI_SUCCESS, "MPI_Send()");
     }
     if (rank + 1 < size)
     {
         int_return = MPI_Send(u_k[rows - 2U] + 1, (int) m_y - 2, MPI_DOUBLE,
             rank + 1, 0, MPI_COMM_WORLD);
-        err_if(int_return != MPI_SUCCESS, err_0);
+        exit_if(int_return != MPI_SUCCESS, "MPI_Send()");
     }
-
-err_0:
-    return int_return;
 }
 
-static int recv(const int size, const int rank,
+static void recv(const int size, const int rank,
     const size_t rows, const size_t m_y, dbl (* const restrict u_k)[m_y])
 {
-    int int_return = MPI_SUCCESS;
+    int int_return;
     if (rank)
     {
         int_return = MPI_Recv(u_k[0] + 1, (int) m_y - 2, MPI_DOUBLE,
-            rank - 1, 0, MPI_COMM_WORLD);
-        err_if(int_return != MPI_SUCCESS, err_0);
+            rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        exit_if(int_return != MPI_SUCCESS, "MPI_Recv()");
     }
     if (rank + 1 < size)
     {
         int_return = MPI_Recv(u_k[rows - 1] + 1, (int) m_y - 2, MPI_DOUBLE,
-            rank + 1, 0, MPI_COMM_WORLD);
-        err_if(int_return != MPI_SUCCESS, err_0);
+            rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        exit_if(int_return != MPI_SUCCESS, "MPI_Recv()");
     }
-
-err_0:
-    return int_return;
 }
 
-static int file_write(const int size, const int rank,
+static void file_write(const int size, const int rank,
     const size_t rows, const size_t m_y, dbl (* const restrict u_k)[m_y],
     const char * const restrict filename)
 {
+    int int_return;
     FILE * restrict file_out;
     if (!rank)
     {
         file_out = fopen(filename, "wb");
-        err_if(!file_out, err_0);
+        exit_if(!file_out, "fopen()");
         int_return = fclose(file_out);
-        err_if(int_return == EOF, err_0);
+        exit_if(int_return == EOF, "fclose()");
     }
 
     int_return = MPI_Barrier(MPI_COMM_WORLD);
-    err_if(int_return != MPI_SUCCESS, err_0);
+    exit_if(int_return != MPI_SUCCESS, "MPI_Barrier()");
 
     const size_t rows_minus_1 = rows - 1, m_y_minus_2 = m_y - 2U;
-    for (int r = 0; i < size; ++i)
+    for (int r = 0; r < size; ++r)
     {
         if (r == rank)
         {
             file_out = fopen(filename, "wb");
-            err_if(!file_out, err_0);
+            exit_if(!file_out, "fopen()");
             for (size_t i = 1; i < rows_minus_1; ++i)
             {
-                size_t size_return = fwrite(u_k[i] + 1, sizeof(dbl), m_y_minus_2, stream);
-                exit_if(size_return != m_y_minus_2, err_1);
+                size_t size_return = fwrite(u_k[i] + 1, sizeof(dbl), m_y_minus_2, file_out);
+                exit_if(size_return != m_y_minus_2, "fwrite()");
             }
             int_return = fclose(file_out);
-            err_if(int_return == EOF, err_0);
+            exit_if(int_return == EOF, "fclose()");
         }
         int_return = MPI_Barrier(MPI_COMM_WORLD);
-        err_if(int_return != MPI_SUCCESS, err_0);
+        exit_if(int_return != MPI_SUCCESS, "MPI_Barrier()");
     }
-
-    return MPI_SUCCESS;
-
-err_1:
-    fclose(file_out);
-err_0:
-    return MPI_ERR_LASTCODE;
 }
